a5fd8de42a204b38ed8e73e68384b34117f4e7c4
Jason Harris <jason.b.harris1@gmail.com>
2020-06-02 17:12:29 -0500

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<style>
+ img {
+	 max-width: 500px;
+	 margin: 0 10%;
+ }
+ h1 {
+	 margin: 0 5%;
+ }
+</style>
+
+<svelte:head>
+	<title>My Sapper Starter</title>
+</svelte:head>
+
+<main>
+	<h1>Hello, and Welcome to my Sapper Starter Kit</h1>
+	<img src="./welcome.jpg" alt="Happiness" />
+	<p>To play around with this page, just traverse to src/routes/index.svelte
+	and feel free to change things up to see how quickly the page refreshes when
+	you save a file.</p>
+	<p>Happy Coding!</p>
+</main>
